37a7f5edebe0a5f53471ce7868746721235a3b85
AriBarrette <arianebarrette@hotmail.com>
2023-04-04 11:31:20 -0400

    gros brouillon

---
+++ scripts/Visualisation_collab.R
@@ -0,0 +1,16 @@
+library(igraph)
+# Asigner une interaction à 10% des paires de noeuds
+matrice_interact <- matrix(0, nr = 162, nc = 162)
+for (i in 1:162) {
+  x=runif(1)
+  if (x>0.5) y=y+1
+  else z=z+1 }
+
+#matrice: ajout de titres de colonnes q
+mafonction<-function(x in 1:162){
+  if data[3,x]>1
+  matrix[x,]
+}
+
+L[runif(S*S) < C] = 1
+sum(L)
