2ec7ba2f0c4d95258af99482b44d9fb4320a6163
AriBarrette <arianebarrette@hotmail.com>
2023-04-15 11:43:07 -0400

    Finalisation du script menant à la création d'une carte représentant les liens entre les étudiants des différentes régions. Pour ce faire, il y a également mise à jour de la requête SQL. Suppression de fichiers inutiles

--- scripts/figure_carte.R
+++ scripts/figure_carte.R
@@ -2,11 +2,14 @@
 library(leaflet)
 
 # Charger les données du réseau de connexion étudiant
-nb_paire_colla
+nb_collab_region
+nb_collab_region_full<-subset(nb_collab_region,region_administrative_et1 !="NA")
+nb_collab_region_full<-subset(nb_collab_region_full,region_administrative_et2 !="NA")
+etudiants_coordo
 
 # Charger la carte
 map <- leaflet() %>%
-  setView(lng = -74.010742, lat = 46.763033, zoom = 5.1) %>%
+  setView(lng = -74.010742, lat = 46.763033, zoom = 5.7) %>%
   addTiles()
 map
 # Ajouter les marqueurs pour les étudiants
@@ -17,10 +20,27 @@ map <- map %>%
              popup = ~prenom_nom,
              radius=1)
 map
-# Ajouter les lignes pour les connexions À MODIFIER
-map <- map %>%
-  addPolylines(data = nb_paire_colla,
-               lng = ~c(longitude_start, longitude_end),
-               lat = ~c(latitude_start, latitude_end),
-               color = "red",
-               weight = 2)
+
+# Ajouter des lignes reliant les régions
+for (i in seq_along(nb_collab_region_full$region_administrative_et1)) {
+  region_administrative_et1 <- nb_collab_region_full$region_administrative_et1[i]
+  region_administrative_et2 <- nb_collab_region_full$region_administrative_et2[i]
+  longitude_et1 <-nb_collab_region_full$longitude_et1[i]
+  latitude_et1 <- nb_collab_region_full$latitude_et1[i]
+  longitude_et2 <-nb_collab_region_full$longitude_et2[i]
+  latitude_et2 <- nb_collab_region_full$latitude_et2[i]
+  
+  map <- map %>%
+    addPolylines(lng = c(longitude_et1, longitude_et2),
+                 lat = c(latitude_et1, latitude_et2),
+                 color="blue",
+                 weight=1)
+}
+map
+
+#Télécharger et ouvrir les librairies nécessaires
+library(htmlwidgets)
+library(webshot)
+
+saveWidget(map, file = "figures/carte_liens.html", selfcontained = FALSE)
+webshot("figures/carte_liens.html", file = "figures/carte_liens.png", vwidth = 800, vheight = 600)
