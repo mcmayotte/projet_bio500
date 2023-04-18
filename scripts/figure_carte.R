63ce6a8c4cb719dc79cb5c99ff5ecd345e932de1
AriBarrette <arianebarrette@hotmail.com>
2023-04-18 11:31:59 -0400

    Finalisation de la carte

--- scripts/figure_carte.R
+++ scripts/figure_carte.R
@@ -1,5 +1,6 @@
 # Importer la bibliothèque
 library(leaflet)
+library(leaflegend)
 #Avoir fait les requêtes SQL
 # Charger les données du réseau de connexion étudiant
 nb_collab_region
@@ -22,7 +23,7 @@ interaction_range <- range(nb_collab_region$nb_collab)
 
 # Définir une fonction pour convertir les nombres d'interactions en épaisseurs de ligne
 get_weight <- function(x) {
-  x / 10  # Diviser par 10 pour normaliser l'épaisseur
+  x / 10  # Diviser par 10 pour normaliser l'épaisseur}
 }
 
 # Ajouter des lignes reliant les régions
@@ -33,25 +34,26 @@ for (i in seq_along(nb_collab_region$region_administrative_et1)) {
   latitude_et1 <- nb_collab_region$latitude_et1[i]
   longitude_et2 <-nb_collab_region$longitude_et2[i]
   latitude_et2 <- nb_collab_region$latitude_et2[i]
-  
   # Obtenir le nombre d'interactions pour la paire de régions actuelle
   interactions <- nb_collab_region$nb_collab[i]
   # Calculer l'épaisseur de la ligne en fonction du nombre d'interactions
-  weight <- get_weight(interactions)
+  weight <- get_weight(interactions) 
   map <- map %>%
     addPolylines(lng = c(longitude_et1, longitude_et2),
                  lat = c(latitude_et1, latitude_et2),
-                 color = "blue",
-                 weight=weight)
+                 col = "blue",
+                 weight =weight,
+                 data=nb_collab_region)
+    
 }
-
 map
 
-#trouver comment mettre la légende. Voir le prof ?
+
 
 #Télécharger et ouvrir les librairies nécessaires
 library(htmlwidgets)
 library(webshot)
+library(ggplot2)
 
 saveWidget(map, file = "figures/carte_liens.html", selfcontained = FALSE)
 webshot("figures/carte_liens.html", file = "figures/carte_liens.png", vwidth = 800, vheight = 600)
